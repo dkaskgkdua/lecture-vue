8c7ab3c16d3c888ed7f1de511bd74e40fae79f90
SongMinJun <dkaskgkdua@gmail.com>
2021-03-04 04:01:03 +0900

    검색 결과 구현 - 검색 결과가 검색폼 아래 위치 - 검색 결과가 보임 - x버튼을 클릭하면 검색폼 초기화, 결과 사라짐

--- src/1-vanilla/js/controllers/MainController.js
+++ src/1-vanilla/js/controllers/MainController.js
@@ -1,4 +1,7 @@
 import FormView from '../views/FormView.js'
+import ResultView from '../views/ResultView.js'
+import SearchModel from '../models/SearchModel.js'
+
 
 const tag = '[MainController]'
 
@@ -8,14 +11,32 @@ export default {
         // 체인닝을 사용했는데 FormView에서 setup시 return this를 해줘야함.
         FormView.setup(document.querySelector('form'))
             .on('@submit', e => this.onSubmit(e.detail.input))
-            .on('@reset', e => this.onClick(e.detail.input))
+            .on('@reset', e => this.onResetForm(e.detail.input))
+
+        ResultView.setup(document.querySelector('#search-result'))
+    },
+
+    search(query) {
+        console.log(tag, 'search()', query)
+        // search api
+        SearchModel.list(query).then(data => {
+            this.onSearchResult(data)
+        })
+
+
     },
 
     onSubmit(input) {
         console.log(tag, 'onSubmit()', input)
+        this.search(input)
     },
 
-    onClick(input) {
+    onResetForm(input) {
         input.value = ''
+        ResultView.hide()
+    },
+
+    onSearchResult(data) {
+        ResultView.render(data)
     }
 }
