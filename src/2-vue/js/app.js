3acc9ecf4251f57390d354b8a0b7f24e19ba3c1e
SongMinJun <dkaskgkdua@gmail.com>
2021-03-09 22:31:39 +0900

    검색 결과 구현 - 검색 결과가 검색폼 아래에 위치한다. - 검색 결과가 보인다 - x버튼 클릭 시 검색폼이 초기화 되고, 검색 결과가 사라진다.

--- src/2-vue/js/app.js
+++ src/2-vue/js/app.js
@@ -1,20 +1,31 @@
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
     el: '#app',
     data : {
         msg: 'hello world',
-        query: ''
+        query: '',
+        submitted: false,
+        searchResult: []
     },
     methods : {
         onSubmit(e) {
-
+            this.search()
         },
         onReset() {
             this.query = ''
             // todo 검색결과를 숨기기
-            debugger
+            this.submitted = false
+            this.searchResult = []
         },
         onKeyup() {
             if(!this.query.length) this.onReset()
+        },
+        search() {
+            SearchModel.list().then(data => {
+                this.submitted = true
+                this.searchResult = data
+            })
         }
     }
 })
