56d30990d3c8f5f1b0db83ee3f4bf1374c6a1d9c
SongMinJun <dkaskgkdua@gmail.com>
2021-03-14 16:50:07 +0900

    1. Result Component 구현 2. List Component 구현    - watch    - computed

--- src/3-component/js/app.js
+++ src/3-component/js/app.js
@@ -2,6 +2,8 @@ import SearchModel from './models/SearchModel.js'
 import KeywordModel from './models/KeywordModel.js'
 import HistoryModel from './models/HistoryModel.js'
 import FormComponent from './components/FormComponent.js';
+import ResultComponent from './components/ResultComponent.js';
+import ListComponent from './components/ListComponent.js';
 
 new Vue({
     el: '#app',
@@ -16,7 +18,9 @@ new Vue({
         history : []
     },
     components: {
-        'search-form': FormComponent
+        'search-form': FormComponent,
+        'search-result' : ResultComponent,
+        'list' : ListComponent
     },
     created() {
         this.selectedTab = this.tabs[0]
