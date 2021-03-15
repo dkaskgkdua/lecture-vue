de3ebeea29946fe4bc913863db1ba78beb621103
SongMinJun <dkaskgkdua@gmail.com>
2021-03-15 21:44:08 +0900

    1. 단일 컴포넌트 구성 2. webpack으로 구성

---
+++ src/4-single-file-component/src/components/TabComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="tabs">
+    <li v-for="tab in tabs" v-on:click="onClickTab(tab)" v-bind:class="{active: tab === selectedTab}">
+      {{ tab }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props : ['tabs', 'selected-tab'],
+  methods : {
+    onClickTab(tab) {
+      this.$emit('@change', tab)
+    }
+  }
+}
+</script>
