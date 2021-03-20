b3901219789d78ec960585158f5fb422f0d36ae0
SongMinJun <dkaskgkdua@gmail.com>
2021-03-20 13:54:51 +0900

    중첩 라우트 구성 및 watch로 실시간 데이터 반영

--- src/5-lecture-vue-trello/src/components/Board.vue
+++ src/5-lecture-vue-trello/src/components/Board.vue
@@ -2,6 +2,11 @@
   <div>
     Board
     <div>bid : {{bid}}</div>
+    <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+    <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+    <hr/>
+    <!-- 중첩 라우팅을 구현하기 위해(Card)-->
+    <router-view></router-view>
   </div>
 </template>
 
