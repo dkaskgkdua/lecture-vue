135493d5ed13ab885a7566b47393a1a26f7bb580
SongMinJun <dkaskgkdua@gmail.com>
2021-03-21 02:14:52 +0900

    네비게이션 가드 사용 - 특정 url 접근 전 체크로직   (ex. 인증정보 -> token)

--- src/5-lecture-vue-trello/src/router/index.js
+++ src/5-lecture-vue-trello/src/router/index.js
@@ -9,15 +9,21 @@ import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter)
 
+const requireAuth = (to, from, next) => {
+   const isAuth = localStorage.getItem('token')
+   const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+   isAuth ? next() : next(loginPath)
+}
+
 // 선언된 순서대로 탐
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/' , component: Home },
+    { path: '/' , component: Home, beforeEnter: requireAuth },
     { path: '/login', component: Login },
     // 중첩 라우팅 설정
-    { path: '/b/:bid', component: Board, children : [
-        { path : 'c/:cid', component: Card }
+    { path: '/b/:bid', component: Board, beforeEnter: requireAuth , children : [
+        { path : 'c/:cid', component: Card, beforeEnter: requireAuth }
       ] },
     { path: '*', component: NotFound }
   ]
