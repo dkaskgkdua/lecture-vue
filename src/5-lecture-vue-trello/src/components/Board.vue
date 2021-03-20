82761dfa6a1e900d1eb1b9a8a474a9d620181ca8
SongMinJun <dkaskgkdua@gmail.com>
2021-03-20 17:30:01 +0900

    데이터 불러오는 구조설계

--- src/5-lecture-vue-trello/src/components/Board.vue
+++ src/5-lecture-vue-trello/src/components/Board.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     Board
-    <div>bid : {{bid}}</div>
-    <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
-    <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+    <div v-if="loading">loading board...</div>
+    <div v-else>
+      <div>bid : {{bid}}</div>
+      <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+      <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+    </div>
+
     <hr/>
     <!-- 중첩 라우팅을 구현하기 위해(Card)-->
     <router-view></router-view>
@@ -15,11 +19,23 @@
 export default {
   data() {
     return {
-      bid: 0
+      bid: 0,
+      loading: false
     }
   },
   created() {
-    this.bid = this.$route.params.bid
+    this.fetchData()
+
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      setTimeout(() => {
+        this.bid = this.$route.params.bid
+        this.loading = false
+      }, 500)
+
+    }
   }
 }
 </script>
--- src/5-lecture-vue-trello/src/components/Card.vue
+++ src/5-lecture-vue-trello/src/components/Card.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     Card
-    <div>cid: {{ cid }}</div>
+    <div v-if="loading">loading card...</div>
+    <div v-else>cid: {{ cid }}</div>
   </div>
 </template>
 
@@ -9,16 +10,32 @@
 export default {
   data() {
     return {
-      cid: 0
+      cid: 0,
+      loading: false
     }
   },
   watch: {
-    '$route'() {
-      this.cid = this.$route.params.cid
+    // 방법 1
+    // '$route'() {
+    //   this.fetchData()
+    // }
+    // 방법 2
+    '$route': {
+      handler: 'fetchData',
+      immediate: true // 즉시 실행 옵션(created랑 중복)
     }
   },
-  created() {
-    this.cid = this.$route.params.cid
+  // created() {
+  //   this.fetchData()
+  // },
+  methods: {
+    fetchData() {
+      this.loading = true
+      setTimeout(() => {
+        this.cid = this.$route.params.cid
+        this.loading = false
+      }, 500)
+    }
   }
 }
 </script>
