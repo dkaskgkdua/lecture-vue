9f95a910519b3f72c7d431f9de58cac01f2d39c4
SongMinJun <dkaskgkdua@gmail.com>
2021-03-20 23:04:14 +0900

    axios 설치 및 기본 사용

--- src/5-lecture-vue-trello/src/components/Home.vue
+++ src/5-lecture-vue-trello/src/components/Home.vue
@@ -7,6 +7,7 @@
       <div v-else>
         Api result: {{apiRes}}
       </div>
+      <div v-if="error"><pre>{{error}}</pre></div>
 
       <ul>
         <li>
@@ -21,11 +22,14 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
       loading: false,
-      apiRes: ''
+      apiRes: '',
+      error : ''
     }
   },
   created() {
@@ -35,20 +39,31 @@ export default {
     fetchData() {
       this.loading= true
 
-      const req = new XMLHttpRequest()
+      axios.get('http://localhost:3000/_health')
+        .then(res => {
+          this.apiRes = res.data
+        })
+        .catch(res => {
+          this.error = res.response.data
+        })
+        .finally(() => {
+          this.loading= false
+        })
 
-      req.open('GET','http://localhost:3000/health')
-
-      req.send()
-
-      req.addEventListener('load', () => {
-        this.loading= false
-        this.apiRes = {
-          status: req.status,
-          statusText: req.statusText,
-          response: JSON.parse(req.response)
-        }
-      })
+      // const req = new XMLHttpRequest()
+      //
+      // req.open('GET','http://localhost:3000/health')
+      //
+      // req.send()
+      //
+      // req.addEventListener('load', () => {
+      //   this.loading= false
+      //   this.apiRes = {
+      //     status: req.status,
+      //     statusText: req.statusText,
+      //     response: JSON.parse(req.response)
+      //   }
+      // })
     }
   }
 }
