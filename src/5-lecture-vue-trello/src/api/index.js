6a290156da652144618fa9db1b3af0ee865dd483
SongMinJun <dkaskgkdua@gmail.com>
2021-03-21 23:27:07 +0900

    로그인 api 연동 및 구현 - localStorage에 토큰정보 저장 - axios 헤더에 인증정보 저장( Bearer )

--- src/5-lecture-vue-trello/src/api/index.js
+++ src/5-lecture-vue-trello/src/api/index.js
@@ -4,7 +4,7 @@ import router from '../router'
 const DOMAIN = 'http://localhost:3000'
 const UNAUTHORIZED = 401
 const onUnauthorized = () => {
-  router.push('/login')
+  router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`)
 }
 
 const request = (method, url, data) => {
@@ -15,13 +15,26 @@ const request = (method, url, data) => {
   }).then(result => result.data)
     .catch(result => {
       const {status} = result.response
-      if(status === UNAUTHORIZED) return onUnauthorized()
-      throw Error(result)
+      if(status === UNAUTHORIZED) onUnauthorized()
+      throw result.response
     })
 }
+// 인증 토큰 정보 세팅
+export const setAuthInHeader = token => {
+  axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
+}
+
+const {token} = localStorage
+if (token) setAuthInHeader(token)
 
 export const board = {
   fetch() {
     return request('get', '/boards')
+  }
+}
+
+export const auth = {
+  login(email, password) {
+    return request('post', '/login', {email, password})
   }
 }
